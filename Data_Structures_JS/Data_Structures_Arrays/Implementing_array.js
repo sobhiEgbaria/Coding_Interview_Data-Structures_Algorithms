0f7fa459b2f249fb9f81bcb566064744f49002ca
sobhiEgbaria <sobhim686@gmail.com>
2025-05-14 14:52:20 +0300

    merge sorted array

--- Data_Structures_JS/Data_Structures_Arrays/Implementing_array.js
+++ Data_Structures_JS/Data_Structures_Arrays/Implementing_array.js
@@ -8,8 +8,36 @@ class MyArray {
   get(index) {
     return this.data[index];
   }
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    delete this.data[this.length - 1];
+    this.length--;
+    return this.length;
+  }
+  delete(index) {
+    this.shiftItems(index);
+    return this.data[index];
+  }
+
+  shiftItems(index) {
+    // [0, 1, 2, 3, 4, 5, 6, 7];
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 const array = new MyArray();
-
-console.log(array.get[0]);
+array.push("you");
+array.push("are");
+array.push("!");
+array.push("nice");
+console.log(array);
+array.delete(2);
+console.log(array);
